d96ed8281866695cd9d4ad127e9150d7edd2dc6e
Sahil <guptasahilkumar783@gmail.com>
2026-03-07 02:02:44 +0530

    Fix IndexError when searching a non-existent book

--- app.py
+++ app.py
@@ -26,6 +26,10 @@ def recommend_ui():
 @app.route('/recommend_books',methods=['post'])
 def recommend():
     user_input = request.form.get('user_input')
+
+    if user_input not in pt.index:
+        return render_template('recommend.html', error="Book not found in our database. Please check the spelling or try another book.")
+
     index = np.where(pt.index == user_input)[0][0]
     similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:5]
 
